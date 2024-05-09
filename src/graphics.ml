e15a1f0a3a6cd48c986145eb47b995c5b35482a6
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2024-05-09 12:17:49 +0200

    add [width] and [height] to [Graphics.Canvas]

--- src/graphics.ml
+++ src/graphics.ml
@@ -4,4 +4,10 @@ module Canvas = struct
   let create ?sketch ?renderer w h =
     let renderer = Jv.of_option ~none:Jv.null Jv.Id.to_jv renderer in
     Sketch.call sketch "createCanvas" [| Jv.of_int w; Jv.of_int h; renderer |]
+
+  let width ?sketch () =
+    Sketch.get sketch "width" |> Jv.to_float
+
+  let height ?sketch () =
+    Sketch.get sketch "height" |> Jv.to_float
 end
