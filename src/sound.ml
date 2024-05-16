d46fe0265bc55dac4c84202a6f086fc3137cbaa6
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2024-05-16 08:53:52 +0200

    sound.ml formatting

--- src/sound.ml
+++ src/sound.ml
@@ -1,6 +1,7 @@
 open Brr
 
-(** Bindings to the {{:https://p5js.org/reference/#/libraries/p5.sound}p5.sound library} *)
+(** Bindings to the
+    {{:https://p5js.org/reference/#/libraries/p5.sound} p5.sound library} *)
 
 module AudioIn = struct
   type t = Jv.t
@@ -8,19 +9,17 @@ module AudioIn = struct
   let make () =
     let p5 = Jv.get (Window.to_jv G.window) "p5" in
     let audio_in = Jv.get p5 "AudioIn" in
-    Jv.new' audio_in [| |]
+    Jv.new' audio_in [||]
 
-  let start audio_in fn =
-    Jv.call audio_in "start"  [| Jv.repr fn |] |> ignore
+  let start audio_in fn = Jv.call audio_in "start" [| Jv.repr fn |] |> ignore
 
-  type stream  = Jv.t
+  type stream = Jv.t
 
-  let stream audio_in : stream =
-    Jv.get audio_in "stream"
+  let stream audio_in : stream = Jv.get audio_in "stream"
 
   let get_level ?smoothing audio_in =
-    let smoothing = Jv.of_option ~none:Jv.null Jv.of_float smoothing  in
-    Jv.call audio_in "getLevel"  [| smoothing |] |> Jv.to_float
+    let smoothing = Jv.of_option ~none:Jv.null Jv.of_float smoothing in
+    Jv.call audio_in "getLevel" [| smoothing |] |> Jv.to_float
 end
 
 type audio_context = Jv.t
